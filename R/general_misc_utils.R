5b3e982ff991e4d32a7288ab7746590f20d88716
NicWir <43075653+NicWir@users.noreply.github.com>
2022-07-04 19:16:54 +0200

    bug fixes

--- R/general_misc_utils.R
+++ R/general_misc_utils.R
@@ -130,3 +130,9 @@ suppress_warnings <- function(.expr, .f, ...) {
     )
   )
 }
+
+base_breaks <- function(n = 10){
+  function(x) {
+    axisTicks(log10(range(x, na.rm = TRUE)), log = TRUE, n = n)
+  }
+}
