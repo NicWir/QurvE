ab71b0c1fb5e3a6174b0d14a11b40b76a63d9fa9
NicWir <43075653+NicWir@users.noreply.github.com>
2022-08-22 15:43:18 +0200

    Compatibility with shiny_app

--- R/growth_plots.R
+++ R/growth_plots.R
@@ -1420,10 +1420,14 @@ plot.grofit <- function(grofit, ...,
                         out.nm = NULL
 )
 {
-  # Convert range arguments
-  x.lim <- unlist(str_split(gsub(";[[:space:]]+", ";", gsub("[[:space:]]+;", ";", x.lim)), pattern = ";|,"))
-  y.lim <- unlist(str_split(gsub(";[[:space:]]+", ";", gsub("[[:space:]]+;", ";", y.lim)), pattern = ";|,"))
-  y.lim.deriv <- unlist(str_split(gsub(";[[:space:]]+", ";", gsub("[[:space:]]+;", ";", y.lim.deriv)), pattern = ";|,"))
+  # Convert range  and selecting arguments
+  names <- unlist(str_split(gsub("[;,][[:space:]]+", ";", gsub("[[:space:]]+[;,]", ";", names)), pattern = ";"))
+  conc <- unlist(str_split(gsub("[;,][[:space:]]+", ";", gsub("[[:space:]]+[;,]", ";", conc)), pattern = "[;,]"))
+  exclude.nm <- unlist(str_split(gsub("[;,][[:space:]]+", ";", gsub("[[:space:]]+[;,]", ";", exclude.nm)), pattern = ";"))
+  exclude.conc <- unlist(str_split(gsub("[;,][[:space:]]+", ";", gsub("[[:space:]]+[;,]", ";", exclude.conc)), pattern = ";"))
+  x.lim <- unlist(str_split(gsub("[;,][[:space:]]+", ";", gsub("[[:space:]]+[;,]", ";", x.lim)), pattern = ";"))
+  y.lim <- unlist(str_split(gsub("[;,][[:space:]]+", ";", gsub("[[:space:]]+[;,]", ";", y.lim)), pattern = ";"))
+  y.lim.deriv <- unlist(str_split(gsub("[;,][[:space:]]+", ";", gsub("[[:space:]]+[;,]", ";", y.lim.deriv)), pattern = ";"))
   suppressWarnings(assign("x.lim" ,as.numeric(x.lim)))
   if(all(is.na(x.lim))) x.lim <- NULL
   suppressWarnings(assign("y.lim" ,as.numeric(y.lim)))
