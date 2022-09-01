cb88e62d7a834b3c937cfe24669182b67c9da7b1
jonfunk21 <funk.jonathan21@gmail.com>
2022-09-01 15:20:18 +0200

    download for growth

--- inst/shiny_app/app.R
+++ inst/shiny_app/app.R
@@ -3639,7 +3639,8 @@ server <- function(input, output, session){
   # Visualize ####
     ## Growth Plots: #####
       ### Group Plots ####
-  output$growth_group_plot <- renderPlot({
+
+  growth_group_plot <- reactive({
     results <- results$growth
     plot.grofit(results,
                 data.type = input$data_type_growth_group_plots,
@@ -3660,6 +3661,10 @@ server <- function(input, output, session){
                 basesize = input$base_size_growth_group_plot,
                 shiny = TRUE
     )
+  })
+
+  output$growth_group_plot <- renderPlot({
+    growth_group_plot()
   })
 
       ### DR Plots ####
@@ -3813,36 +3818,39 @@ server <- function(input, output, session){
 
   output$downloads_growth_group_plot <- downloadHandler(
     filename = function() {
-      paste("data-", Sys.Date(), ".csv", sep="")
+      paste("test_plot", ".png", sep="")
     },
     content = function(file) {
-      out.nm <- gsub(paste0("^.+", paste(.Platform$file.sep)), "", file)
-      out.dir <- gsub(paste0("[^", .Platform$file.sep, "]+$"), "", file)
-      file.format <- gsub("^.+\\.", "", file)
-
-      results <- results$growth
-      plot.grofit(results,
-                  data.type = input$data_type_growth_group_plots,
-                  names = input$select_samples_based_on_string_growth_group_plots,
-                  conc = input$select_samples_based_on_concentration_growth_group_plots,
-                  exclude.nm = input$exclude_samples_based_on_string_growth_group_plots,
-                  exclude.conc = input$exclude_samples_based_on_concentration_growth_group_plots,
-                  mean = input$plot_group_averages_growth_group_plots,
-                  deriv = input$plot_derivative_growth_group_plots,
-                  log.y = input$log_transform_y_axis_growth_group_plots,
-                  x.lim = c(input$x_range_min_growth_group_plot, input$x_range_max_growth_group_plot),
-                  y.lim = c(input$y_range_min_growth_group_plot,input$y_range_max_growth_group_plot),
-                  y.lim.deriv = c(input$y_range_min_derivative_growth_group_plot, input$y_range_max_derivative_growth_group_plot),
-                  y.title = input$y_axis_title_growth_group_plot,
-                  x.title = input$x_axis_title_growth_group_plot,
-                  y.title.deriv = input$y_axis_title_derivative_growth_group_plot,
-                  lwd = input$line_width_growth_group_plot,
-                  basesize = input$base_size_growth_group_plot,
-                  shiny = TRUE,
-                  export = TRUE,
-                  out.nm = out.nm,
-                  out.dir = out.dir
-      )
+      # out.nm <- gsub(paste0("^.+", paste(.Platform$file.sep)), "", file)
+      # out.dir <- gsub(paste0("[^", .Platform$file.sep, "]+$"), "", file)
+      # file.format <- gsub("^.+\\.", "", file)
+      #
+      # results <- results$growth
+      # plot.grofit(results,
+      #             data.type = input$data_type_growth_group_plots,
+      #             names = input$select_samples_based_on_string_growth_group_plots,
+      #             conc = input$select_samples_based_on_concentration_growth_group_plots,
+      #             exclude.nm = input$exclude_samples_based_on_string_growth_group_plots,
+      #             exclude.conc = input$exclude_samples_based_on_concentration_growth_group_plots,
+      #             mean = input$plot_group_averages_growth_group_plots,
+      #             deriv = input$plot_derivative_growth_group_plots,
+      #             log.y = input$log_transform_y_axis_growth_group_plots,
+      #             x.lim = c(input$x_range_min_growth_group_plot, input$x_range_max_growth_group_plot),
+      #             y.lim = c(input$y_range_min_growth_group_plot,input$y_range_max_growth_group_plot),
+      #             y.lim.deriv = c(input$y_range_min_derivative_growth_group_plot, input$y_range_max_derivative_growth_group_plot),
+      #             y.title = input$y_axis_title_growth_group_plot,
+      #             x.title = input$x_axis_title_growth_group_plot,
+      #             y.title.deriv = input$y_axis_title_derivative_growth_group_plot,
+      #             lwd = input$line_width_growth_group_plot,
+      #             basesize = input$base_size_growth_group_plot,
+      #             shiny = TRUE,
+      #             export = TRUE,
+      #             out.nm = out.nm,
+      #             out.dir = out.dir
+      # )
+      png(file=file)
+      plot(growth_group_plot())
+      dev.off()
     }
   )
 
