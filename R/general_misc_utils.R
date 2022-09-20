0080e418e24d629dbbfd1f8b88f172793fcf8459
NicWir <43075653+NicWir@users.noreply.github.com>
2022-09-20 21:28:06 +0200

    updated Namespace

--- R/general_misc_utils.R
+++ R/general_misc_utils.R
@@ -211,9 +211,8 @@ zipFastener <- function(df1, df2, along=2)
 #'
 #' @importFrom DT dataTableOutput renderDT datatable
 #' @importFrom readxl read_excel excel_sheets
-#' @importFrom shinythemes shinytheme
 #'
-#' @import shiny shinyjs shinycssloaders shinyFiles doParallel knitr kableExtra
+#' @import shiny doParallel knitr kableExtra
 run_app <- function() {
 
   # Locate all the shiny apps that exist
